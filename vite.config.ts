2da71ec173d619d8d4518cba5629663e7a93ec5a
songhyunjung <sss3593@naver.com>
2025-05-28 17:37:58 +0900

    big fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,5 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
-
-// Vitest를 위한 타입 지원 임포트
-import type { UserConfigExport } from 'vite';
 
 
 export default defineConfig({
